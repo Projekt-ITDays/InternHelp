fe6fb6411e28391f479f9b20bd0d405439131456
Dyzio96 <bartoszbranka1@gmail.com>
2026-03-17 21:21:55 +0100

    cleaned

---
+++ frontend/src/app/prompt-component/prompt-component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Ai } from '../service/ai';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-prompt-component',
+  imports: [FormsModule],
+  templateUrl: './prompt-component.html',
+  styleUrl: './prompt-component.css',
+})
+export class PromptComponent {
+  prompt = '';
+  response = ''; 
+  loading = false;
+
+  constructor(private ai: Ai, private cdr: ChangeDetectorRef) {}
+
+  sendRequest() {
+    if (!this.prompt.trim()) return;
+
+    this.loading = true;
+    this.response = ''; 
+    const currentPrompt = this.prompt;
+    this.prompt = '';
+
+    this.ai.askGemini(currentPrompt).subscribe({
+      next: (data: any) => {
+      this.response = data.answer; 
+      this.loading = false;
+      this.cdr.detectChanges();
+      }
+    });
+  }
+}
